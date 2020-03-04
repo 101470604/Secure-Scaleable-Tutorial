4508bfb26b7ee983b21e1a14fce26c9e17453a5b
Tom Wright <101470604@student.swin.edu.au>
2020-03-04 17:33:29 +1100

    2.1 Partial Progress

---
+++ 2.1P/EDJBC/src/edjbc/MyDB.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edjbc;
+
+import java.sql.Statement;
+import java.io.IOException;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Connection;
+
+/**
+ *
+ * @author Tom
+ */
+public class MyDB {
+
+    public void createMyuserTable() {
+        Connection cnnct = null;
+        Statement stmnt = null;
+        try {
+            cnnct = getConnection();
+            stmnt = cnnct.createStatement();
+            stmnt.execute("CREATE TABLE MYUSER ( "
+                    + " UserId CHAR(6) CONSTRAINT PK_CUSTOMER PRIMARY KEY, "
+                    + " Name CHAR(30), Password CHAR(6), Email CHAR(30), "
+                    + " Phone CHAR(10), Address CHAR(60), "
+                    + " SecQn CHAR(60), SecAns CHAR(60))");
+        } catch (SQLException ex) {
+            while (ex != null) {
+                ex.printStackTrace();
+                ex = ex.getNextException();
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (stmnt != null) {
+                try {
+                    stmnt.close();
+                } catch (SQLException e) {
+                }
+            }
+            if (cnnct != null) {
+                try {
+                    cnnct.close();
+                } catch (SQLException sqlEx) {
+                }
+            }
+        }
+    }
+
+    public static Connection getConnection() throws SQLException,
+            IOException {
+        System.setProperty("jdbc.drivers", "org.apache.derby.jdbc.ClientDriver");
+        String url = "jdbc:derby://localhost/sun-appservsamples;create = true";
+        String username = "APP";
+        String password = "APP";
+        return DriverManager.getConnection(url, username, password);
+    }
+
+    public void dropMyuserTable() {
+        Connection cnnct = null;
+        Statement stmnt = null;
+        try {
+            cnnct = getConnection();
+            stmnt = cnnct.createStatement();
+            stmnt.execute("DROP TABLE MYUSER");
+        } catch (SQLException ex) {
+            while (ex != null) {
+                ex.printStackTrace();
+            }
+            ex = ex.getNextException();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (stmnt != null) {
+                try {
+                    stmnt.close();
+                } catch (SQLException e) {
+                }
+            }
+            if (cnnct != null) {
+                try {
+                    cnnct.close();
+                } catch (SQLException sqlEx) {
+                }
+            }
+        }
+    }
+}
